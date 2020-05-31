d8f3235a78f97b234502b2e4387dcb5e611b55b5
Samuel Hegner <C16439262@mydit.ie>
2020-05-31 15:25:27 +0100

    Component Creation Start

---
+++ Workout App/Assets/Scripts/AdjustToSliderValue.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using TMPro;
+using UnityEngine;
+
+public class AdjustToSliderValue : MonoBehaviour
+{
+    [SerializeField] private TextMeshProUGUI _text;
+    
+    public void AdjustTextToSliderValue(float value)
+    {
+        _text.text = value.ToString();
+    }
+}
---
+++ Workout App/Assets/Scripts/LimitInputToSeconds.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using TMPro;
+using UnityEngine;
+
+public class LimitInputToSeconds : MonoBehaviour
+{
+    [SerializeField] private TMP_InputField _textToAdjust;
+
+    public void AdjustValueToSeconds(string value)
+    {
+        if (value == String.Empty)
+            return;
+
+        //convert string to int
+        print(value);
+
+        bool parsable = int.TryParse(value, out int intValue);
+
+        string changedValue = "";
+
+        if (parsable)
+        {
+            if (intValue > 59)
+            {
+                intValue = 59;
+                changedValue = intValue.ToString();
+            }
+            else if (intValue < 10)
+            {
+                changedValue = "0" + intValue;
+            }
+            else
+            {
+                changedValue = intValue.ToString();
+            }
+        }
+
+
+        _textToAdjust.text = changedValue;
+        print(_textToAdjust.text);
+    }
+}
