eb0398f5f0268ce62909f9c616c6de71f1b5dec8
Samuel Hegner <C16439262@mydit.ie>
2020-05-31 23:45:48 +0100

    Workout Saving Started (loading unfinished)

---
+++ Workout App/Assets/Scripts/SaveManager.cs
@@ -0,0 +1,109 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.IO;
+using System.Runtime.Serialization.Formatters.Binary;
+using UnityEngine;
+
+public class SaveManager : MonoBehaviour
+{
+    public static SaveManager instance;
+
+    public static string persistentDataPath;
+
+    void Awake()
+    {
+        instance = this;
+        persistentDataPath = Application.persistentDataPath + "/workout_save";
+
+        LoadAllWorkouts();
+    }
+
+    public bool IsSaveFile()
+    {
+        return Directory.Exists(persistentDataPath);
+    }
+
+
+    public void SaveAllWorkouts()
+    {
+        for(int i = 0; i < WorkoutList.instance.workouts.Count; i++)
+        {
+            SaveWorkout(WorkoutList.instance.workouts[i], i);
+        }
+    }
+
+    public void SaveWorkout(WorkoutSO workoutSo, int index)
+    {
+        if (!IsSaveFile())
+        {
+            Directory.CreateDirectory(persistentDataPath);
+        }
+
+
+        string workoutPath = persistentDataPath + "/workout_"+ index;
+
+        if (!Directory.Exists(workoutPath))
+        {
+            Directory.CreateDirectory(workoutPath);
+        }
+
+        BinaryFormatter bf = new BinaryFormatter();
+
+        FileStream file = File.Create(workoutPath + "/" + workoutSo.name + ".txt");
+
+        var json = JsonUtility.ToJson(workoutSo);
+
+        bf.Serialize(file, json);
+
+        file.Close();
+
+        for (int i = 0; i < workoutSo._components.Count; i++)
+        {
+            string componentPath = workoutPath + "/" + workoutSo._components[i]._name;
+
+            if (!Directory.Exists(componentPath))
+            {
+                Directory.CreateDirectory(componentPath);
+            }
+
+            BinaryFormatter bf2 = new BinaryFormatter();
+
+            FileStream componentFile = File.Create(componentPath + "/component_" + i + ".txt");
+
+            var json2 = JsonUtility.ToJson(workoutSo._components[i]);
+
+            bf2.Serialize(componentFile, json2);
+            componentFile.Close();
+        }
+    }
+
+    public void LoadAllWorkouts()
+    {
+        if (!IsSaveFile())
+            return;
+
+        int indexChecker = 0;
+        
+        string workoutPath = persistentDataPath + "/workout_"+ indexChecker;
+        
+        while (Directory.Exists(workoutPath))
+        {
+            //TODO deserialize the workout and all of its components and push it into the WorkoutList list
+            string[] files = Directory.GetFiles(workoutPath);
+
+            foreach (var name in files)
+            {
+                print(name);
+            }
+            
+            indexChecker++;
+            workoutPath = persistentDataPath + "/workout_"+ indexChecker;
+        }
+
+    }
+
+    public void LoadWorkout()
+    {
+        
+    }
+}
---
+++ Workout App/Assets/Scripts/WorkoutList.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class WorkoutList : MonoBehaviour
+{
+    public List<WorkoutSO> workouts;
+
+    public static WorkoutList instance;
+
+    private void Awake()
+    {
+        instance = this;
+    }
+}
